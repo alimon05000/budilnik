d668bed5ace4333032208a17425c560116a79a83
alimon05000 <alimsrifrv23@gmail.com>
2026-03-20 15:52:06 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('tahajjud-store').then((cache) => cache.addAll([
+    caches.open('tahajjud-store-premium').then((cache) => cache.addAll([
       '/',
       '/index.html',
       '/style.css',
@@ -12,4 +12,23 @@ self.addEventListener('install', (e) => {
 
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request)));
+});
+
+// Обработка клика по фоновому уведомлению
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then(function(clientList) {
+      if (clientList.length > 0) {
+        let client = clientList[0];
+        for (let i = 0; i < clientList.length; i++) {
+          if (clientList[i].focused) {
+            client = clientList[i];
+          }
+        }
+        return client.focus();
+      }
+      return clients.openWindow('/');
+    })
+  );
 });
